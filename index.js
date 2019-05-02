ecc304df189cbaa40789db93fb152c60b9b9be6d
AntonVoronezh <pstut@mail.ru>
2019-05-02 19:26:06 +0300

    add forEach

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ Button.addEventListener('click', request);
 function request() {
 	console.log('request');
 }
+
+$.ajax('https://repetitora.net/api/JS/Images', {
+    success: function(data) {
+        // console.log('i get request', data);
+        data.forEach(el => {
+            const img = document.createElement('img');
+            img.src = el.thumbnail;
+            resultBlock.appendChild(img);
+        });
+    }
+});
