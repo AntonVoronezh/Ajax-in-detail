1c0ee1fdcd401265240ef9e65e8ea4b9ca62406b
AntonVoronezh <pstut@mail.ru>
2019-05-02 20:23:02 +0300

    add service.js

--- index.js
+++ index.js
@@ -6,14 +6,3 @@ Button.addEventListener('click', () => {
 	makeRequest();
 });
 
-function makeRequest() {
-	$.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`, {
-		success: function(data) {
-			data.forEach(el => {
-				const img = document.createElement('img');
-				img.src = el.thumbnail;
-				resultBlock.appendChild(img);
-			});
-		},
-	});
-}
