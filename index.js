3787efb2cb08ddecd52c2f5d5873684411ff7345
AntonVoronezh <pstut@mail.ru>
2019-05-02 20:34:47 +0300

    refactoring functions

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// UI
 const resultBlock = document.querySelector('#result');
 const pageNumber = document.querySelector('#number');
 const Button = document.querySelector('#click');
 
 Button.addEventListener('click', () => {
-	makeRequest();
+	getImages(pageNumber.value, onDataReceived);
 });
+
+function onDataReceived(data) {
+    data.forEach(el => {
+        const img = document.createElement('img');
+        img.src = el.thumbnail;
+        resultBlock.appendChild(img);
+    });
+};
 
--- service.js
+++ service.js
@@ -1,11 +1,18 @@
-function makeRequest() {
-	$.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`, {
-		success: function(data) {
-			data.forEach(el => {
-				const img = document.createElement('img');
-				img.src = el.thumbnail;
-				resultBlock.appendChild(img);
-			});
-		},
+// DAL - data access layer
+
+// function getImages(pageNumber) {
+// 	$.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`, {
+// 		success: function(data) {
+// 			data.forEach(el => {
+// 				const img = document.createElement('img');
+// 				img.src = el.thumbnail;
+// 				resultBlock.appendChild(img);
+// 			});
+// 		},
+// 	});
+// }
+function getImages(pageNumber, successCalback) {
+	$.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`, {
+        success: successCalback
 	});
 }
